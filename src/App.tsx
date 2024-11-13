0e6f0be887b783155c17bcd5ca644c2b14914203
Caio Vinícius <caio1525pereira@gmail.com>
2024-11-13 17:53:26 -0300

    chore: :truck: Organização na estrutura do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import { Home } from "./view/pages/Home";
+
 const queryClient = new QueryClient({});
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <h1>Teste - Tech 4 Humans</h1>
+      <Home />
     </QueryClientProvider>
   );
 }
---
+++ src/view/pages/Home.tsx
@@ -0,0 +1,3 @@
+export function Home() {
+  return <h1>Teste - Tech 4 Humans</h1>;
+}
