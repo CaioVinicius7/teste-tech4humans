388ef9062de9e18326c1cd0a9c5b02894679724e
Caio Vinícius <caio1525pereira@gmail.com>
2024-11-14 19:52:04 -0300

    refactor: :goal_net: Tratativa para data inválida

--- src/app/utils/dateFormatter.ts
+++ src/app/utils/dateFormatter.ts
@@ -1,8 +1,12 @@
-import { format, parse } from "date-fns";
+import { format, isValid, parse } from "date-fns";
 import { ptBR } from "date-fns/locale";
 
 export function dateFormatter(date: string) {
   const parsedDate = parse(date, "yyyy-MM-dd", new Date());
+
+  if (!isValid(parsedDate)) {
+    return "Invalid date";
+  }
 
   return format(parsedDate, "dd/MM/yyyy", { locale: ptBR });
 }
