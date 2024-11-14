a374bed314d30c631145b9d6bf9451b876d5b32f
Caio Vinícius <caio1525pereira@gmail.com>
2024-11-14 02:32:00 -0300

    refactor: :lipstick: Troca de flex por grid na estilização da listagem de filmes

--- src/view/pages/Home/index.tsx
+++ src/view/pages/Home/index.tsx
@@ -6,12 +6,12 @@ export function Home() {
   const { movies, isFetching, handlePaginate } = useHomeController();
 
   return (
-    <main className="max-w-[1420] px-10 w-full mx-auto py-10">
+    <main className="max-w-[1420px] px-10 w-full mx-auto py-10">
       {!movies && <span>Ops! Nenhum filme foi encontrado...</span>}
 
       {isFetching && <span>Carregando...</span>}
 
-      <div className="flex items-center justify-center flex-wrap gap-4">
+      <div className="grid grid-cols-[repeat(auto-fit,_minmax(14rem,1fr))] gap-4">
         {!!movies &&
           movies.movies.map((movie) => (
             <MovieCard key={movie.id} movie={movie} />
